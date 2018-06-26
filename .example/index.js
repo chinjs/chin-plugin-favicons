37e1aa18f15b5aaac3a0d0fc30389b6d9c93ac99
kthjm <toxictoxer@gmail.com>
2018-06-26 15:33:24 +0900

    Change favicons source png => svg

--- .example/index.js
+++ .example/index.js
@@ -1,5 +1,5 @@
-import { join } from 'path'
 import favicons from '..'
+import { join } from 'path'
 import { outputFile } from 'fs-extra'
 import generateHtml from './generateHtml.js'
 
@@ -8,16 +8,16 @@ const out = join(__dirname, 'out')
 
 const nameAsDir = true
 const config = { path: nameAsDir ? './favicons' : './' }
-const ext = favicons({ nameAsDir, config })
+const svg2fav = favicons({ nameAsDir, config })
 
 export default {
   put,
   out,
   clean: true,
-  processors: { png: ext },
+  processors: { svg: svg2fav },
   after: () => {
     const outpath = join(out, 'index.html')
-    const htmlTags = ext.after()
+    const htmlTags = svg2fav.after()
     const IndexHtml = generateHtml(htmlTags)
     return outputFile(outpath, IndexHtml)
   }
--- test.js
+++ test.js
@@ -7,29 +7,36 @@ import { promisify } from 'util'
 const readFile = promisify(fs.readFile)
 
 const name = 'favicons'
-const filepath = resolve(join('.example', 'put', `${name}.png`))
-const out = parse(resolve(join('.example', 'out', `${name}.png`)))
+const readPromise = readFile(resolve(join('.example', 'put', `${name}.svg`)))
+const out = parseXbase(resolve(join('.example', 'out', `${name}.svg`)))
 
 const test = (opts) => () => {
 
   const { processor, after } = favicons(opts)
 
-  return readFile(filepath)
-  .then(buffer => processor(buffer, { out }))
-  .then(result => {
-    assert.ok(Array.isArray(result))
-    assert.ok(Array.isArray(after()))
+  return (
+    readPromise
+    .then(data => processor(data, { out }))
+    .then(result => {
+      assert.ok(Array.isArray(result))
+      assert.ok(Array.isArray(after()))
 
-    const nameAsDir = Boolean((opts || {}).nameAsDir)
-    
-    result.forEach(([outpath]) => {
-      const dirSplits = dirname(outpath).split(sep)
-      const isNameAsDir = dirSplits[dirSplits.length - 1] === name
-      assert.equal(nameAsDir, isNameAsDir)
+      const nameAsDir = Boolean((opts || {}).nameAsDir)
+
+      result.forEach(([outpath]) => {
+        const dirSplits = dirname(outpath).split(sep)
+        const isNameAsDir = dirSplits[dirSplits.length - 1] === name
+        assert.equal(nameAsDir, isNameAsDir)
+      })
     })
-  })
+  )
 }
 
 it('favicons()', test())
 it('favicons({ nameAsDir: false })', test({ nameAsDir: false }))
 it('favicons({ nameAsDir: true })', test({ nameAsDir: true }))
+
+function parseXbase(pathstring) {
+  const { root, dir, name, ext } = parse(pathstring)
+  return { root, dir, name, ext }
+}
